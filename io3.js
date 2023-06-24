ea4f8015249931f8c331f53fee9c523d94d9b855
selvamani2001 <ak5091527@gmail.com>
2023-06-24 16:37:48 +0530

    first commit

---
+++ io3.js
@@ -0,0 +1,26 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin,
+  output:process.stdout,
+
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+  console.log(userInput);
+  
+
+
+
+  //end-here
+});
